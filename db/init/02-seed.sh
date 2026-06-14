367f71226fb78453d47c141837ae116d31bddc88
Wander Hayashida <wrhbra2013@gmail.com>
2026-06-14 10:09:21 -0300

    v0.3.8

--- db/init/02-seed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    INSERT INTO login (usuario, senha, isadmin)
-    VALUES ('${ADMIN_EMAIL:-admin@amoranimal.ong.br}', '${ADMIN_PASS:-@admin}', true)
-    ON CONFLICT (usuario) DO NOTHING;
-EOSQL
