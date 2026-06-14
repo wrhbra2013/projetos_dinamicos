6266c1d84ac5747d82aaeceaec81fe7b50c17d58
Wander Hayashida <wrhbra2013@gmail.com>
2026-06-14 09:14:43 -0300

    v0.3.7

--- static/js/main.js
+++ static/js/main.js
@@ -111,8 +111,9 @@ function phoneFormat(input) {
     }
     if (digits.length === 0) return '';
     if (digits.length <= 2) return '(' + digits + ')';
-    if (digits.length <= 7) return '(' + digits.slice(0, 2) + ') ' + digits.slice(2);
-    return '(' + digits.slice(0, 2) + ') ' + digits.slice(2, 7) + '-' + digits.slice(7);
+    if (digits.length <= 6) return '(' + digits.slice(0, 2) + ') ' + digits.slice(2);
+    if (digits.length <= 10) return '(' + digits.slice(0, 2) + ') ' + digits.slice(2, 6) + ' - ' + digits.slice(6);
+    return '(' + digits.slice(0, 2) + ') ' + digits.slice(2, 7) + ' - ' + digits.slice(7);
 }
 
 function phoneValidator(input) {
